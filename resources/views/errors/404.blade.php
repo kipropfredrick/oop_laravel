ba2c011c1ab891edeeba8a15a103d277996e07e0
Levidic <levkisaka@gmail.com>
2021-04-06 19:22:32 +0300

    Display third level category only if it exists

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.app')
 
+@section('title', 'Page not found')
+
 @section('content')
 
 
     <div class="container">
-        <div class="text-center">
-            <img style="height:50%;width:50%;object-fit:contain;" src="{{asset('images/404.png')}}" alt="">
+        <div class="col-md-4">
+
+            <div class="text-center">
+                <img class="img-fluid" style="height:50%;width:50%;object-fit:contain;" src="{{asset('images/404.png')}}" alt="">
+            </div>
+        
         </div>
     </div>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -423,7 +423,23 @@
 
     </script>
 
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jscroll/2.4.1/jquery.jscroll.min.js"></script>
+
+
+    <script type="text/javascript">
+        $('ul.pagination').hide();
+        $(function() {
+            $('.scrolling-pagination').jscroll({
+                autoTrigger: true,
+                padding: 0,
+                nextSelector: '.pagination li.active + li a',
+                contentSelector: 'div.scrolling-pagination',
+                callback: function() {
+                    $('ul.pagination').remove();
+                }
+            });
+        });
+    </script>
 
     
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
