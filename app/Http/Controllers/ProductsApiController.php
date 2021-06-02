f97cd0bbe07fc7d8f8aa2c6b27aa5a0f36f39588
Brian Mutiso <brianqmutiso.com>
2021-06-02 00:30:54 -0700

    update

--- app/Http/Controllers/ProductsApiController.php
+++ app/Http/Controllers/ProductsApiController.php
@@ -182,7 +182,7 @@ function myAccount(Request $request){
 
           $phone="254".substr($username, 1);
         $existingCustomer = \App\Customers::where('phone','=',$phone)->first();
-          $email= \App\User::whereUser_id( $existingCustomer->user_id)->first()->email;
+          $email= \App\User::whereId( $existingCustomer->user_id)->first()->email;
 
         }
         elseif($valid_email == 1){
