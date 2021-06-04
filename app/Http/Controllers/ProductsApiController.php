519d65a80e7ea9fddf8472c51638f0b023622a93
Brian Mutiso <brianqmutiso.com>
2021-06-04 01:03:20 -0700

    update

--- app/Http/Controllers/ProductsApiController.php
+++ app/Http/Controllers/ProductsApiController.php
@@ -66,7 +66,7 @@ for ($i=0; $i <count($result) ; $i++) {
         $subcat['subcategory']=$subcat;
         $thirdlevelcategory=ThirdLevelCategory::whereSubcategory_id($subcategories[$j]->id)->get();
   foreach($thirdlevelcategory as $thirdcategory){
-            
+            return Products::with('gallery')->whereThird_level_category_id ($thirdcategory->id)->first();
             $thirdlevelcategory['icon'] = Products::with('gallery')->whereThird_level_category_id ($thirdcategory->id)->first()['gallery']->image_path;
         }
 
