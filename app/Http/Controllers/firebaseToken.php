2b1ecaab50244accc1e5f9614a3bc51a6e3738b2
Brian Mutiso <brianqmutiso.com>
2021-06-15 21:59:56 -0700

    update

--- app/Http/Controllers/firebaseToken.php
+++ app/Http/Controllers/firebaseToken.php
@@ -10,7 +10,7 @@ class firebaseToken extends Controller
     function insertToken(Request $request){
     	$phone=$request->input('username');
     	$token=$request->input("token");
-    	$existingCustomer = \App\Customers::where('username','=',$phone)->first();
+    	$existingCustomer = \App\Customers::where('phone','=',$phone)->first();
         $userid= $existingCustomer->userid;
 \App\User::whereId( $user_id)->update(['token'=>$token]);
 
@@ -18,7 +18,7 @@ class firebaseToken extends Controller
  function updateToken(Request $request){
     	$phone=$request->input('username');
     	$token=$request->input("token");
-    	$existingCustomer = \App\Customers::where('username','=',$phone)->first();
+    	$existingCustomer = \App\Customers::where('phone','=',$phone)->first();
         $userid= $existingCustomer->userid;
 \App\User::whereId( $user_id)->update(['token'=>$token]);
 
