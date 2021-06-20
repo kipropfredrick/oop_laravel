cbadfdb1567fa1087e1e767cd054524389434cd7
Brian Mutiso <brianqmutiso.com>
2021-06-19 21:40:08 -0700

    update

--- app/Http/Controllers/autApi.php
+++ app/Http/Controllers/autApi.php
@@ -15,23 +15,7 @@ use \App\Mail\SendRegistrationEmail;
 use App\Http\Controllers\pushNotification;
 class autApi extends Controller
 {
-    //
-
-            private function get_msisdn_network($msisdn){
-            $regex =  [
-                 'airtel' =>'/^\+?(254|0|)7(?:[38]\d{7}|5[0-6]\d{6})\b/',
-                 'equitel' => '/^\+?(254|0|)76[0-7]\d{6}\b/',
-                 'safaricom' => '/^\+?(254|0|)(?:7[01249]\d{7}|1[01234]\d{7}|75[789]\d{6}|76[89]\d{6})\b/',
-                 'telkom' => '/^\+?(254|0|)7[7]\d{7}\b/',
-             ];
-         
-             foreach ($regex as $operator => $re ) {
-                 if (preg_match($re, $msisdn)) {
-                     return [preg_replace('/^\+?(254|0)/', "254", $msisdn), $operator];
-                 }
-             }
-             return [false, false];
-         }
+    
 
 
     function registerUser(Request $request){
