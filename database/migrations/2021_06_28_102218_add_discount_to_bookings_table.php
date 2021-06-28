42bddb4a2ba4420f6dbb2ffa03d738e4f9175626
Brian Mutiso <brianqmutiso.com>
2021-06-28 02:22:09 -0700

    xx

--- database/migrations/2021_06_28_102218_add_discount_to_bookings_table.php
+++ database/migrations/2021_06_28_102218_add_discount_to_bookings_table.php
@@ -15,7 +15,7 @@ class AddDiscountToBookingsTable extends Migration
     {
         Schema::table('bookings', function (Blueprint $table) {
             //
-            $table->integer("discount");
+            $table->integer("discount")->default(0);
         });
     }
 
