617278bc1ad850b2f58af5c435bff8d11e623bfe
Brian Mutiso <brianqmutiso.com>
2021-07-08 15:02:30 -0700

    update

---
+++ app/Http/Controllers/TopupsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Customers;
+
+class TopupsController extends Controller
+{
+    //
+
+public function balances(Request $request){
+	$phone=$request->phone;
+  $customers=Customers::wherePhone($phone)->first();
+
+if($customers==null){
+return Array("data"=>Array("response"=>"An Error Occured Processing Your Request"),"error"=>true);
+}
+$balance=intval(DB::table("users")->whereId($customers->user_id)->first()->balance);
+
+
+return Array("balance"=>$balance,"error"=>false);
+}
+
+
+}
