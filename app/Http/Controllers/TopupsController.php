813a862f77ba63d18dfaa802113ada19cdf4b42b
Brian Mutiso <brianqmutiso.com>
2021-07-08 15:56:49 -0700

    update

--- app/Http/Controllers/TopupsController.php
+++ app/Http/Controllers/TopupsController.php
@@ -19,7 +19,7 @@ if($customers==null){
 return Array("data"=>Array("response"=>"Account Association Failed.contact support"),"error"=>true);
 }
 $balance=intval(DB::table("users")->whereId($customers->user_id)->first()->balance);
-
+wss
 
 return Array("error"=>false,"data"=>Array("response"=>"Account linked successfully.","balance"=>$balance));
 }
