fde06b05a9ce8e4ec8f467c185de8c3a87447ae5
Brian Mutiso <brianqmutiso.com>
2021-07-13 01:27:18 -0700

    xx

---
+++ database/migrations/2021_07_13_112553_add_notified_at_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNotifiedAtToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->timestamp('notified_at')->default('2020-01-01');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
