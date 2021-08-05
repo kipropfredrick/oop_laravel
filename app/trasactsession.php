dba46462f50adf78c51cfa357a7cbb9a014e9a00
Brian Mutiso <brianqmutiso.com>
2021-08-05 17:42:12 +0800

    pesa transact encr

---
+++ app/trasactsession.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class trasactsession extends Model
+{
+    //
+    
+        protected $fillable = [
+        'requestId', 'sessionId',
+    ];
+}
