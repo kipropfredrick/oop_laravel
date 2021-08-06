0ee2fa055003ad3c1b7e8459b3891bdb27ec8fe4
Brian Mutiso <brianqmutiso.com>
2021-08-06 15:10:53 +0800

    xxx

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2336,7 +2336,7 @@ if ($request->validmpesa!=null) {
              if($request->ajax()){ 
 
 
-        $payments = \App\Payments::with('customer','mpesapayment','customer.user','product:id,product_name,product_code','booking')->whereIn("id",$validmpesa)->orderBy('id', 'DESC');
+        $payments = \App\Payments::with('customer','mpesapayment','customer.user','product:id,product_name,product_code','booking')->whereIn("payments.id",$validmpesa)->orderBy('payments.id', 'DESC');
        
             
 
