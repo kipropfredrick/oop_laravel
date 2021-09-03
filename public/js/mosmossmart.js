211e38126ec2eaf9a8a39e141a29208a982134b8
Brian Mutiso <brianqmutiso.com>
2021-09-03 18:50:17 +0800

    ...

--- public/js/mosmossmart.js
+++ public/js/mosmossmart.js
@@ -1,6 +1,23 @@
 
+
+var scripts = document.getElementsByTagName('script');
+var lastScript = scripts[scripts.length-1];
+var scriptName = lastScript.src;
+//alert("loading: " + lastScript.getAttribute('one'));
+var url_string = scriptName; 
+var url = new URL(url_string);
+var secret_key = url.searchParams.get("secret_key");
+
+
+
 var image="";
 var itemName=""
+var onApprovefunc= function(data){
+  
+};
+var onDeclinefunc=function(data){
+};
+var price=0;
 // Create new link Element
         var link = document.createElement('link'); 
   
@@ -16,7 +33,7 @@ var itemName=""
         document.getElementsByTagName('HEAD')[0].appendChild(link); 
 
 
-include('https://code.jquery.com/jquery-3.3.1.slim.min.js', function() {
+include('https://code.jquery.com/jquery-3.6.0.js', function() {
     $(document).ready(function() {
 
 
@@ -38,10 +55,13 @@ $('#mosmos').attr("data-target", "#mosmosmodal");
 $('#mosmos').attr("data-toggle",'modal');
 
 var s=`
-
 <div class="modal fade" id="mosmosmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog  modal-lg modal-dialog-centered" role="document">
     <div class="modal-content">
+
+
+
+
       <div class="modal-header text-center">
         
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -51,10 +71,12 @@ var s=`
       <div class="modal-body">
       <div class="site__body">
         
+           
         <div style="margin-top:20px" class="checkout block">
             <div class="container">
                 <div class="row">
                                                 <!-- features -->
+
             <div class="col-sm-4">
 
                     <div class="mdgf">
@@ -71,7 +93,7 @@ var s=`
 
                                 <div class="">
                                 <div>
-                                <p>You are making a booking for <strong><span id="productName"></span></strong>. Minimum deposit is <strong>KSh.500</strong>.</p>
+                               <p>You are making a booking for <strong><span id="productName"></span></strong>. Minimum deposit is <strong>KSh.500</strong>.</p>
                          
                             </div>
                                       
@@ -82,7 +104,7 @@ var s=`
                                 
                                 <div class="form-row">
                                     <div class="form-group col-md-12">
-                                    <form action="/make-booking" method="post">
+                                    <form action="https://mosmos.co.ke/api/smartchekout" method="post" id="mosmos-form">
                                        
                                         <input type="hidden" name="quantity" value="1">
                                         <input type="hidden" name="product_id" value="1">
@@ -121,7 +143,7 @@ var s=`
                                           <div class="form-group col-md-6">
                                             <label for="checkout-company-name">County</label><span style="color:red">*</span>
                                             
-                                              <select id="counties" class="form-control js-example-basic-single" name="county_id" placeholder="Enter name" type="text" class="form-control " onchange="filter()" required>
+                                              <select id="counties" class="form-control js-example-basic-single" name="county_id" placeholder="Enter name" type="text" class="form-control "  required>
                                                 <option value="">Select/search county</option>
                                                <option value="1">Mombasa</option>
                                                 </select>
@@ -153,8 +175,34 @@ var s=`
                                                     </div>
                                                 </div>
                                             </div>
-                                                <button type="submit" class="btn btn-primary">Book Now</button>
+                                                <button type="submit" class="btn btn-primary">Make Booking</button>
                                           
+
+
+
+      <div class="col-12 overlay" style="position:absolute;display:none;" id="spinner">
+      <div class="d-flex justify-content-center">  
+      
+
+        <span class="fa fa-spinner fa-spin fa-4x " style="color: orange;"></span></div>
+      </div>
+    </div>
+
+ <style type="text/css">
+  .overlay {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    z-index: 1000;
+    top: 40%;
+    left: 0px;
+    opacity: 1.0;
+    filter: alpha(opacity=50);
+ }
+
+ </style>
+
+
                                             </form>
                                               </div>
                                             <div class="card-divider"></div>
@@ -166,6 +214,7 @@ var s=`
         
   </div>
 </div>
+
 <style>
 .mdg-features {
     background: #333;
@@ -178,6 +227,52 @@ var s=`
 `;
 
 $('body').append(s);
+// this is the id of the form
+$("#mosmos-form").submit(function(e) {
+
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+$("#spinner").show();
+    var form = $(this);
+
+var productName='<input type="hidden" name="name" value="'+itemName+'">';
+var keyInput='<input type="hidden" name="secret_key" value="'+secret_key+'">';
+var productPrice='<input type="hidden" name="productPrice" value="'+price+'">';
+
+// var productPrice='<input type="hidden" name="name" value="'+productPrice+'">';
+// var productPrice='<input type="hidden" name="name" value="'+productPrice+'">';
+form.append(productName);
+form.append(keyInput);
+form.append(productPrice);
+    var url = form.attr('action');
+    
+    $.ajax({
+           type: "POST",
+           url: url,
+           data: form.serialize(), // serializes the form's elements.
+           success: function(data)
+           {
+            $("#spinner").hide();
+            if(data.status){
+              $("#mosmosmodal").modal('hide');
+              onApprovefunc(data);
+                      console.log(data);
+
+            }
+            else{
+              onDeclinefunc(data);
+                      console.log(data);
+            }
+              // alert(JSON.stringify(data)); // show response from the php script.
+           },
+           error:function(data){
+            console.log(data);
+            alert("An error occured processing your request");
+            $("#spinner").hide();
+           }
+         });
+
+    
+});
 
       document.getElementById("mosmosproductimage").src =image;
        document.getElementById("productName").innerHTML =itemName;
@@ -201,12 +296,7 @@ $('body').append(s);
 
 //
 include('https://kit.fontawesome.com/a076d05399.js', function() {
-    $(document).ready(function() {
-          //const mosmosobj = new mosmos("Lipa Mos Mos");
-
-
-    });
-}); 
+ }); 
 
 
 
@@ -232,14 +322,21 @@ function include(filename, onload) {
 
 
 class mosmos {
-  constructor(name="Lipa Mosmos",productName="product name not set correctly",imageSource="not set") {
+  constructor( {name="Lipa Mosmos",productName="product name not set correctly",
+    imageSource="not set",productPrice=0,onApprove=function(data){alert("sasa")}, onDecline=function(data){}} = {}) {
     this.name = name;
     document.getElementById("mosmos").innerHTML =name;
     image=imageSource;
     itemName=productName;
+    onApprovefunc=onApprove;
+    onDeclinefunc=onDecline;
+    price=productPrice;
 
   }
+
 }
+
+
 
 
 
