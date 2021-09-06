6cf2f5c8937f996a0f1f0ebd3eb841602deedbac
Brian Mutiso <brianqmutiso.com>
2021-09-06 17:30:49 +0800

    ...

--- public/js/mosmossmart.js
+++ public/js/mosmossmart.js
@@ -55,6 +55,7 @@ $('#mosmos').attr("data-target", "#mosmosmodal");
 $('#mosmos').attr("data-toggle",'modal');
 
 var s=`
+
 <div class="modal fade" id="mosmosmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog  modal-lg modal-dialog-centered" role="document">
     <div class="modal-content">
@@ -62,10 +63,10 @@ var s=`
 
 
 
-      <div class="modal-header text-center">
+      <div class=" text-center mr-3 mt-2">
         
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
+          <span aria-hidden="true" style="color:red;font-weight: bold;">&times;</span>
         </button>
       </div>
       <div class="modal-body">
@@ -82,12 +83,14 @@ var s=`
                     <div class="mdgf">
                         <div class="row" >
                         <img src="not set" id="mosmosproductimage" class="col-12">
+
+                   
                         </div>
                     </div>
               
       
     </div>
-                    <div class="col-12 col-lg-6 col-xl-7">
+                    <div class="col-12 col-lg-6 col-xl-7" id="responsedivmosmos">
                         <div class="card mb-lg-0">
                             <div class="card-body">
 
@@ -104,7 +107,7 @@ var s=`
                                 
                                 <div class="form-row">
                                     <div class="form-group col-md-12">
-                                    <form action="https://mosmos.co.ke/api/smartchekout" method="post" id="mosmos-form">
+                                    <form action="http://mosmos.sale/api/smartchekout" method="post" id="mosmos-form">
                                        
                                         <input type="hidden" name="quantity" value="1">
                                         <input type="hidden" name="product_id" value="1">
@@ -145,7 +148,53 @@ var s=`
                                             
                                               <select id="counties" class="form-control js-example-basic-single" name="county_id" placeholder="Enter name" type="text" class="form-control "  required>
                                                 <option value="">Select/search county</option>
-                                               <option value="1">Mombasa</option>
+                                            <option value="2" class="counties">Mombasa</option>
+                                                                                    <option value="3" class="counties">Kwale</option>
+                                                                                    <option value="4" class="counties">Kilifi</option>
+                                                                                    <option value="5" class="counties">Tana River</option>
+                                                                                    <option value="6" class="counties">Lamu</option>
+                                                                                    <option value="7" class="counties">Taita–Taveta</option>
+                                                                                    <option value="8" class="counties">Garissa</option>
+                                                                                    <option value="9" class="counties">Wajir</option>
+                                                                                    <option value="10" class="counties">Mandera</option>
+                                                                                    <option value="11" class="counties">Marsabit</option>
+                                                                                    <option value="12" class="counties">Isiolo</option>
+                                                                                    <option value="13" class="counties">Meru</option>
+                                                                                    <option value="14" class="counties">Tharaka-Nithi</option>
+                                                                                    <option value="15" class="counties">Embu</option>
+                                                                                    <option value="16" class="counties">Kitui</option>
+                                                                                    <option value="17" class="counties">Machakos</option>
+                                                                                    <option value="18" class="counties">Makueni</option>
+                                                                                    <option value="19" class="counties">Nyandarua</option>
+                                                                                    <option value="20" class="counties">Nyeri</option>
+                                                                                    <option value="21" class="counties">Kirinyaga</option>
+                                                                                    <option value="22" class="counties">Murang&#039;a</option>
+                                                                                    <option value="23" class="counties">Kiambu</option>
+                                                                                    <option value="24" class="counties">Turkana</option>
+                                                                                    <option value="25" class="counties">West Pokot</option>
+                                                                                    <option value="26" class="counties">Samburu</option>
+                                                                                    <option value="27" class="counties">Trans-Nzoia</option>
+                                                                                    <option value="28" class="counties">Uasin Gishu</option>
+                                                                                    <option value="29" class="counties">Elgeyo-Marakwet</option>
+                                                                                    <option value="30" class="counties">Nandi</option>
+                                                                                    <option value="31" class="counties">Baringo</option>
+                                                                                    <option value="32" class="counties">Laikipia</option>
+                                                                                    <option value="33" class="counties">Nakuru</option>
+                                                                                    <option value="34" class="counties">Narok</option>
+                                                                                    <option value="35" class="counties">Kajiado</option>
+                                                                                    <option value="36" class="counties">Kericho</option>
+                                                                                    <option value="37" class="counties">Bomet</option>
+                                                                                    <option value="38" class="counties">Kakamega</option>
+                                                                                    <option value="39" class="counties">Vihiga</option>
+                                                                                    <option value="40" class="counties">Bungoma</option>
+                                                                                    <option value="41" class="counties">Busia</option>
+                                                                                    <option value="42" class="counties">Siaya</option>
+                                                                                    <option value="43" class="counties">Kisumu</option>
+                                                                                    <option value="44" class="counties">Homa Bay</option>
+                                                                                    <option value="45" class="counties">Migori</option>
+                                                                                    <option value="46" class="counties">Kisii</option>
+                                                                                    <option value="47" class="counties">Nyamira</option>
+                                                                                    <option value="48" class="counties">Nairobi</option>
                                                 </select>
                                            
                                                 </div>
@@ -170,12 +219,13 @@ var s=`
                                                     <div class="form-check">
                                                     <input class="form-check-input" type="checkbox" checked id="terms" required>
                                                     <label class="form-check-label" for="terms">
-                                                        I agree to the <a href="/terms" target="_blank">Terms of Service</a> and <a href="/privacy-policy" target="_blank">Privacy Policy</a>.*
+                                                        I agree to the <a href="https://mosmos.co.ke/terms" target="_blank">Terms of Service</a> and <a href="https://mosmos.co.ke/privacy-policy" target="_blank">Privacy Policy</a>.*
                                                     </label>
                                                     </div>
                                                 </div>
                                             </div>
                                                 <button type="submit" class="btn btn-primary">Make Booking</button>
+
                                           
 
 
@@ -186,6 +236,7 @@ var s=`
 
         <span class="fa fa-spinner fa-spin fa-4x " style="color: orange;"></span></div>
       </div>
+
     </div>
 
  <style type="text/css">
@@ -210,7 +261,11 @@ var s=`
                                             </div>
                                         </div>
                                     </div>
-
+   <div style="background-color: blue;">
+                          <a href="https://mosmos.co.ke" target=”_blank”>
+                            <img src="https://mosmos.co.ke/assets/img/logo/web-logo.png" class="col-sm-6 col-lg-4 col-md-4" style="margin-left: auto;margin-right: auto;display: block;" >
+                          </a>
+                      </div>
         
   </div>
 </div>
@@ -253,13 +308,20 @@ form.append(productPrice);
            {
             $("#spinner").hide();
             if(data.status){
-              $("#mosmosmodal").modal('hide');
-              onApprovefunc(data);
+              // $("#mosmosmodal").modal('hide');
+              //onApprovefunc(data);
+              $("#responsedivmosmos").html("<p><strong> "+itemName+"</strong> booked successfuly. Use Paybill <strong>4040299</strong> and account number <strong>test</strong> for all your next payments.</p>"+
+                '<img src="https://t3.ftcdn.net/jpg/03/55/05/12/360_F_355051279_iCFYD4WdBAfkLljJQlyWYDgii03rinlH.jpg" style="width: 100px;height: 100px;margin-left: auto;margin-right: auto;display: block;" style="">');
                       console.log(data);
 
             }
             else{
-              onDeclinefunc(data);
+              //onDeclinefunc(data);
+          
+                   $("#responsedivmosmos").html("<p> "+data.message+"</p> <br>"+
+                '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4cHYs5t8hc25yl3SAA2caI5JDWezWPKligQ&usqp=CAU" style="width: 100px;height: 100px;margin-left: auto;margin-right: auto;display: block;" style="">');
+                      console.log(data);
+
                       console.log(data);
             }
               // alert(JSON.stringify(data)); // show response from the php script.
@@ -321,11 +383,11 @@ function include(filename, onload) {
 }
 
 
-class mosmoscs {
+class mosmos {
   constructor( {name="Lipa Mosmos",productName="product name not set correctly",
     imageSource="not set",productPrice=0,onApprove=function(data){alert("sasa")}, onDecline=function(data){}} = {}) {
     this.name = name;
-    document.getElementById("mosmos").innerHTML ="Lipa mos mos";
+    document.getElementById("mosmos").innerHTML =name;
     image=imageSource;
     itemName=productName;
     onApprovefunc=onApprove;
