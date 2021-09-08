b81116fa7416c0b01a81cec4cc867ab7c8e36e1b
Brian Mutiso <brianqmutiso.com>
2021-09-08 18:46:07 +0800

    ..

--- public/js/mosmossmart.js
+++ public/js/mosmossmart.js
@@ -288,6 +288,20 @@ var s=`
 
 $('body').append(s);
 // this is the id of the form
+    var checkeventcount = 1,prevTarget;
+    $('.modal').on('show.bs.modal', function (e) {
+        if(typeof prevTarget == 'undefined' || (checkeventcount==1 && e.target!=prevTarget))
+        {  
+          prevTarget = e.target;
+          checkeventcount++;
+          e.preventDefault();
+          $(e.target).appendTo('body').modal('show');
+        }
+        else if(e.target==prevTarget && checkeventcount==2)
+        {
+          checkeventcount--;
+        }
+     });
 
 $("#mosmos-form").submit(function(e) {
 
