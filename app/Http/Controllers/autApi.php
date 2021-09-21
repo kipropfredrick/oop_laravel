0577852e6e994e93e36490feb6062f610839f2d9
Brian Mutiso <brianqmutiso.com>
2021-09-21 16:27:56 +0800

    ....

--- app/Http/Controllers/autApi.php
+++ app/Http/Controllers/autApi.php
@@ -951,7 +951,7 @@ $data_string = $ilive.$ioid .$iinv.$iamount.$itel.$ieml.$ivid.$icurr.$p1.$p2.$p3
 // $data_string = "live=".$live."&oid=".$ioid."&inv".$iinv."&amount=".$amount."&tel=".$itel."&email=".$ieml."&vid=".$ivid."&curr=".$icurr."&p1=".$p1."&p2=".$p2."&p3=".$p3."&p4=".$p4."&cbk=".$icbk."&cst=".$icst."&crl".$icrl;
 $hashed = hash_hmac("sha1", $data_string, $hashkey);
 // $datastring = "account=".$account."&amount=".$amount."&biller_name=".$biller_name."&phone=".$phone."&vid=".$IpayId ;
-$url="https://payments.ipayafrica.com/v3/ke?live=" .$ilive ."&mm=1&mb=1&dc=1&cc=1&mer=ipay"."&mpesa=0&airtel=0&equity=0&creditcard=1&elipa=0&debitcard=0" ."&oid=".$ioid."&inv=".$iinv."&ttl=".$iamount."&tel=".$itel."&eml=".$ieml. "&vid=".$ivid ."&p1=".$p1."&p2=".$p2."&p3=".$p3."&p4=".$p4."&crl=".$icrl."&cbk=".$icbk."&cst=".$icst."&curr=".$icurr."&hsh=".$hashed;
+$url="https://payments.ipayafrica.com/v3/ke?live=".$ilive."&mm=1&mb=1&dc=1&cc=1&mer=ipay"."&mpesa=0&airtel=0&equity=0&creditcard=1&elipa=0&debitcard=0"."&oid=".$ioid."&inv=".$iinv."&ttl=".$iamount."&tel=".$itel."&eml=".$ieml."&vid=".$ivid."&p1=".$p1."&p2=".$p2."&p3=".$p3."&p4=".$p4."&crl=".$icrl."&cbk=".$icbk."&cst=".$icst."&curr=".$icurr."&hsh=".$hashed;
 
 
 return Array("url"=>$url,"fee"=>(round(0.035*$trans_amount)),"trans_amount"=>(round(0.035*$trans_amount)+$trans_amount ));
