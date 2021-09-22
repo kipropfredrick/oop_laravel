baa60eea7f72c9eaac8368272c93d76c1c285a30
Brian Mutiso <brianqmutiso.com>
2021-09-22 17:38:11 +0800

    ...

--- app/Http/Controllers/autApi.php
+++ app/Http/Controllers/autApi.php
@@ -986,7 +986,7 @@ function capturepayment(Request $request){
 if ($request->status!="aei7p7yrx4ae34") {
   # code...
 
-$details=Array('txncd'=>'test',"uyt"=>$request->uyt,"agt"=>$request->agt,"qwh"=>$request->qwh,"ifd"=>$request->ifd,"poi"=>$request->poi,"oid"=>$request->oid,"amount"=>$request->p1,"total_amount"=>$request->mc,"channel"=>$request->channel);
+$details=Array('txncd'=>'test',"uyt"=>$request->uyt,"agt"=>$request->agt,"qwh"=>$request->qwh,"ifd"=>$request->ifd,"poi"=>$request->poi,"oid"=>$request->id,"amount"=>$request->p1,"total_amount"=>$request->mc,"channel"=>$request->channel);
 
 Log::info(json_encode($details));
 return 0;
